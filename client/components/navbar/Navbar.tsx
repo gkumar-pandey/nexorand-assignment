371233b1e5e6a72a9a73c05e86b7ae88e86ed705
gkumar-pandey <gautamshekhar078@gmail.com>
2024-11-21 23:06:10 +0530

    feat: navbar created

--- client/components/navbar/Navbar.tsx
+++ client/components/navbar/Navbar.tsx
@@ -1,26 +1,75 @@
 "use client";
 import React from "react";
-
 import Link from "next/link";
 
 import Container from "../container/container";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useAppSelector } from "@/store/hook";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { setUser } from "@/store/slice/userSlice";
 
 const Navbar = () => {
+  const router = useRouter();
+  const user = useAppSelector(state => state.user);
+
+  const logoutHandler = () => {
+    const resetUser = {
+      email: "",
+      firstName: "",
+      lastName: "",
+      token: "",
+      username: "",
+    };
+    localStorage.removeItem("user");
+    router.push("/login");
+    setUser(resetUser);
+    toast.success("Logout successfully.");
+  };
+
   return (
     <nav className="py-4 w-full shadow-md">
       <Container>
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-row gap-3">
             <Link href={"/"}>Home</Link>
-            <Link href={"/login"}>Login</Link>
-            <Link href={"/register"}>Register</Link>
+            <Link href={"/leaderboard"}>Leaderboard</Link>
           </div>
           <div>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage
+                    src={user.firstName ? "" : "https://github.com/shadcn.png"}
+                    alt="@shadcn"
+                  />
+                  <AvatarFallback>
+                    {user?.firstName[0]?.toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>{user?.email}</DropdownMenuItem>
+                <DropdownMenuItem>{user?.firstName}</DropdownMenuItem>
+                <DropdownMenuItem>Points : {user?.Points}</DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Button
+                    onClick={logoutHandler}
+                    className="bg-red-500 text-white w-full hover:bg-red-600">
+                    Logout
+                  </Button>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
       </Container>
